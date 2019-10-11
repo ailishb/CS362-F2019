46cac2a2d048292bac600a1af637858e278e5cb8
Ailish Bateman <batemana@oregonstate.edu>
2019-10-11 16:22:42 -0400

    Moved card effects to individual functions at end of dominion.c

--- projects/batemana/dominion/dominion_helpers.h
+++ projects/batemana/dominion/dominion_helpers.h
@@ -11,5 +11,13 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
+int baronEffect(int choice1, struct gameState *state, int currentPlayer);
+int minionEffect(int choice1, int choice2, struct gameState *state,
+                int handPos, int currentPlayer);
+int ambassadorEffect(int choice1, int choice2, struct gameState *state,
+                    int handPos, int currentPlayer);
+int tributeEffect(struct gameState *state, int nextPlayer, int *tributeRevealedCards, int currentPlayer);
+int mineEffect(int choice1, int choice2, struct gameState *state,
+                int handPos, int currentPlayer);
 
 #endif
